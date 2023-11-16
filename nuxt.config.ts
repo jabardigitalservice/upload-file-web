7a2083aa6324a081a6e29badcfdc518bfe44f7c6
Ray Fajar Salinggih <58242304+rayfajars@users.noreply.github.com>
2023-11-16 10:30:29 +0800

    chore: fix ssr fales put in tailwind.config wkwkwk

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_API_BASE,
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  ssr: false,
   content: [],
   theme: {
     extend: {
